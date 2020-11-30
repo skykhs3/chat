2d8279486b42fcda92f640cd1169eea2bb0b9b33
skykhs3 <skykhs3@naver.com>
2020-12-01 01:03:19 +0900

    로그인 페이지 디자인 완료

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,7 +1,50 @@
 import React from 'react'
+import './LoginPage.css'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 class LoginPage extends React.Component{
     render(){
-        return(<div>RoomPage</div>);
+        return(<div>
+            
+            <div >
+            <div className="navigationBar">SPARCS Newbie Project</div>
+            <div className="navigationBar">Login</div>
+            <div className="navigationBar">Register</div>
+            </div>
+
+            <form className="form">
+            <TextField
+            type="email"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              name="email"
+              label="Email"
+              autoComplete="email"
+              autoFocus
+            //  onChange={onEmailHandler}
+            />
+            <TextField
+            type="password"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              autoComplete="current-password"
+            //  onChange={onPasswordHandler}
+            />
+             <Button
+              type="submit"
+              variant="outlined"
+              fullWidth
+            >Sign In</Button>
+</form>
+
+        </div>);
     }
 
 }
