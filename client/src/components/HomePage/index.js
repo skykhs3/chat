1e0a8f37efba007d07baf8f5561aec93728d9cf1
skykhs3 <skykhs3@naver.com>
2020-12-01 01:24:22 +0900

    채팅방 리스트 디자인 오류남

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
 class HomePage extends React.Component{
+
+    renderRoomList=()=>{
+
+    }
     render(){
-        return(<div>RoomPage</div>);
+        return(<div>
+            <div >
+            <div className="navigationBar">SPARCS Newbie Project</div>
+            <div className="navigationBar">Log out</div>
+            </div>
+            
+            
+            <Button>
+                새로운 방 만들기
+            </Button>
+
+            {this.renderRoomList()}
+
+            <div > 김현수</div>
+                <div> 같이 밥 먹을 사람 구해요~</div>
+            {/* <Button>
+                <div > 김현수</div>
+                <div> 같이 밥 먹을 사람 구해요~</div>
+                <div> 심심해요</div>
+            </Button> */}
+            
+            
+            
+            </div>);
     }
 
 }
